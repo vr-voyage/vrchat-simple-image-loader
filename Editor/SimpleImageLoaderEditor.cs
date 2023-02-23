245be44d9278f0a7f4c94c73bd04eb69251b02e6
Voyage <voyage@miouyouyou.fr>
2023-02-23 04:27:37 +0100

    A Simple Image Loader !
    At last ! We can load PNG from the internet in 2023 !
    SUCH PROWESSES !
    
    Signed-off-by: Voyage <voyage@miouyouyou.fr>

---
+++ Editor/SimpleImageLoaderEditor.cs
@@ -0,0 +1,89 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UdonSharpEditor;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace VRVoyage.SimpleScripts
+{
+    [CustomEditor(typeof(SimpleImageLoader))]
+    public class SimpleImageLoaderEditor : Editor
+    {
+        ReorderableList urlsList;
+
+        SerializedProperty urls;
+        SerializedProperty receivingMaterial;
+        SerializedProperty receivingPanel;
+        SerializedProperty rescalePanel;
+        SerializedProperty errorOutput;
+        SerializedProperty errorTexture;
+        SerializedProperty pictureRefreshTime;
+
+        private void OnEnable()
+        {
+            urls = serializedObject.FindProperty(nameof(SimpleImageLoader.urls));
+            receivingMaterial = serializedObject.FindProperty(nameof(SimpleImageLoader.receivingMaterial));
+            receivingPanel = serializedObject.FindProperty(nameof(SimpleImageLoader.receivingPanel));
+            rescalePanel = serializedObject.FindProperty(nameof(SimpleImageLoader.rescalePanel));
+            errorOutput = serializedObject.FindProperty(nameof(SimpleImageLoader.errorOutput));
+            errorTexture = serializedObject.FindProperty(nameof(SimpleImageLoader.errorTexture));
+            pictureRefreshTime = serializedObject.FindProperty(nameof(SimpleImageLoader.pictureRefreshTime));
+
+            /* Code shamelessly taken from USharpVideoInspector (USharpVideo : https://github.com/MerlinVR/USharpVideo) */
+            urlsList = new ReorderableList(serializedObject, urls, true, true, true, true);
+            urlsList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+            {
+                Rect fieldRect = new Rect(
+                    x: rect.x,
+                    y: rect.y + 2,
+                    width: rect.width,
+                    height: EditorGUIUtility.singleLineHeight);
+
+                EditorGUI.PropertyField(
+                    position: fieldRect,
+                    property: urlsList.serializedProperty.GetArrayElementAtIndex(index),
+                    label: new GUIContent());
+            };
+            urlsList.drawHeaderCallback = (Rect rect) => {
+                EditorGUI.LabelField(
+                    rect,
+                    new GUIContent(
+                        "Images URLS",
+                        "The URLS of the pictures that should be displayed"));
+            };
+
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (UdonSharpGUI.DrawProgramSource(target)) return;
+
+            EditorGUILayout.LabelField("Main Settings", EditorStyles.boldLabel);
+            urlsList.DoLayoutList();
+            EditorGUILayout.PropertyField(
+                pictureRefreshTime,
+                new GUIContent(
+                    "Wait time in seconds",
+                    "Wait time, in seconds, between the last download and the next one."));
+            EditorGUILayout.PropertyField(receivingMaterial);
+            EditorGUILayout.PropertyField(errorTexture);
+
+            EditorGUILayout.LabelField("Additional settings", EditorStyles.boldLabel);
+            EditorGUI.BeginChangeCheck();
+            rescalePanel.boolValue = EditorGUILayout.Toggle(
+                "Rescale the display panel",
+                rescalePanel.boolValue);
+            EditorGUI.EndChangeCheck();
+            EditorGUILayout.PropertyField(receivingPanel, new GUIContent("Display panel"));
+            EditorGUILayout.PropertyField(errorOutput, new GUIContent("(Optional) Show errors in"));
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+
+#endif
---
+++ Runtime/SimpleImageLoader.cs
@@ -0,0 +1,216 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Image;
+using VRC.SDKBase;
+using VRC.Udon.Common.Interfaces;
+
+namespace VRVoyage.SimpleScripts
+{
+    public class SimpleImageLoader : UdonSharpBehaviour
+    {
+        public VRCUrl[] urls;
+        public Material receivingMaterial;
+        public Transform receivingPanel;
+        public bool rescalePanel = true;
+        public TMPro.TextMeshPro errorOutput;
+        public Texture2D errorTexture;
+
+        const float minimumWaitTime = 5; /* seconds */
+        public float pictureRefreshTime = 6; /* seconds */
+
+        int urlIndex = 0;
+        VRCImageDownloader downloader;
+        TextureInfo info;
+
+        Vector3 scale = Vector3.one;
+        float panelScaleX = 1;
+
+        bool loggingErrors = false;
+        /* So 2023 ! */
+        IVRCImageDownload lastError;
+
+        void OnEnable()
+        {
+            downloader = new VRCImageDownloader();
+            if (urls == null || urls.Length == 0)
+            {
+                Debug.LogError($"[{name}] [{this.GetType().Name}] No URLS set. Disabling.");
+                enabled = false;
+                return;
+            }
+
+            if (receivingMaterial == null)
+            {
+
+                Debug.LogError($"[{name}] [{this.GetType().Name}] Material not set. Disabling.");
+                enabled = false;
+                return;
+            }
+
+            if (errorTexture == null)
+            {
+
+                Debug.LogError($"[{name}] [{this.GetType().Name}] Forgot to set the error texture ? Disabling.");
+                enabled = false;
+                return;
+            }
+
+            if (rescalePanel & (receivingPanel == null))
+            {
+
+                Debug.LogError($"[{name}] [{this.GetType().Name}] You asked to rescale the display panel, but forgot to set it. Disabling.");
+                enabled = false;
+                return;
+            }
+
+            if (pictureRefreshTime < minimumWaitTime)
+            {
+                Debug.LogWarning($"[{name}] [{this.GetType().Name}] Wait time too low. Reset to {minimumWaitTime}");
+                pictureRefreshTime = minimumWaitTime;
+                return;
+            }
+
+            loggingErrors = (errorOutput != null);
+
+            info = new TextureInfo();
+            /* Don't repeat the bottom on the top, the left on the right, ... */
+            info.WrapModeU = TextureWrapMode.Clamp;
+            info.WrapModeV = TextureWrapMode.Clamp;
+            if (rescalePanel)
+            {
+                scale = receivingPanel.localScale;
+                panelScaleX = scale.x;
+            }
+
+            DownloadFromCurrentURL();
+
+            if (loggingErrors)
+            {
+                /* Ugh... */
+                CheckForErrors();
+            }
+        }
+
+        #region Entrypoints
+        /* Alright, so, right now, OnImageLoadError is called from a thread,
+         * that cannot access Unity UI.
+         *
+         * So in order to show any error, I need to POLL on an error object
+         * and use it if it is set !
+         * 
+         * I'm basically polling on the equivalent of 'errno' in 2023 because
+         * some people thought it would be a good idea to call errors handlers
+         * from a thread !
+         */
+        public void CheckForErrors()
+        {
+            if (lastError != null)
+            {
+                ShowError();
+            }
+
+            lastError = null;
+            SendCustomEventDelayedSeconds("CheckForErrors", 2);
+        }
+
+        public void DownloadFromNextURL()
+        {
+            urlIndex += 1;
+            DownloadFromCurrentURL();
+        }
+        #endregion
+
+        #region Handle Downloads
+        VRCUrl CurrentURL()
+        {
+            int nUrls = urls.Length;
+            /* Handle +1 / -1 gracefully and avoid OOB errors */
+            /* nUrls == 0 is handled in onEnable */
+            urlIndex = urlIndex < nUrls ? urlIndex : 0;
+            urlIndex = urlIndex >= 0 ? urlIndex : nUrls - 1;
+            return urls[urlIndex];
+        }
+
+        void ScheduleNextDownload()
+        {
+            SendCustomEventDelayedSeconds(
+                "DownloadFromNextURL",
+                pictureRefreshTime,
+                VRC.Udon.Common.Enums.EventTiming.Update);
+        }
+
+        void DownloadFromCurrentURL()
+        {
+            var url = CurrentURL();
+            if (url == null)
+            {
+                ScheduleNextDownload();
+                return;
+            }
+
+            downloader.DownloadImage(
+                CurrentURL(),
+                receivingMaterial,
+                (IUdonEventReceiver)this,
+                info);
+        }
+        #endregion
+
+        #region Success / Fail
+        public override void OnImageLoadSuccess(IVRCImageDownload result)
+        {
+            if (result.State == VRCImageDownloadState.Complete)
+            {
+                var image = result.Result;
+                RescalePanelWidth(((float)image.width / (float)image.height));
+                EnableErrorPanel(false);
+                ScheduleNextDownload();
+            }
+        }
+
+        public override void OnImageLoadError(IVRCImageDownload result)
+        {
+            if (result.State == VRCImageDownloadState.Error)
+            {
+                lastError = result;
+                ScheduleNextDownload();
+            }
+        }
+
+        void RescalePanelWidth(float ratio)
+        {
+            if (!rescalePanel) return;
+            scale.x = panelScaleX * ratio;
+            receivingPanel.localScale = scale;
+        }
+
+        void EnableErrorPanel(bool state)
+        {
+            if (!loggingErrors) return;
+            errorOutput.gameObject.SetActive(state);
+        }
+
+        void ShowError()
+        {
+            if (loggingErrors == false) return;
+            if (lastError == null) return;
+
+            errorOutput.gameObject.SetActive(true);
+            errorOutput.text = $"Error {lastError.Error}\n{lastError.ErrorMessage}";
+
+            var material = lastError.Material;
+            /* Not really trusting VRC objects ... */
+            if (material != null)
+            {
+                material.SetTexture("_MainTex", errorTexture);
+            }
+
+            RescalePanelWidth(1);
+        }
+        #endregion
+
+
+    }
+
+}
